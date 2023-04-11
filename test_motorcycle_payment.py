fb81bfe548a0304a5c690b7c32a7083a8c5b6359
fatt8312 <115389231+fatt8312@users.noreply.github.com>
2023-04-11 23:24:41 +0700

    Update test_motorcycle_payment.py

--- test_motorcycle_payment.py
+++ test_motorcycle_payment.py
@@ -6,19 +6,6 @@ def test_calculate_monthly_payment1():
     # Test case 1: Price 100000, down payment 20000
     assert round(calculate_monthly_payment(100000, 20000), 2) == 6848.6
 
-def test_calculate_monthly_payment2():
-    # Test case 2: Price 50000, down payment 10000
-    assert round(calculate_monthly_payment(50000, 10000), 2) == 3424.3
-
-def test_calculate_monthly_payment3():
-    # Test case 3: Price 150000, down payment 30000
-    assert round(calculate_monthly_payment(150000, 30000), 2) == 10272.9
-
-# Test the calculate_down_Payment function
-def test_minimum_down_input_50000():
-    # Test case 4: Price 50000
-    assert round(calculate_down_Payment(50000), 2) == 10000
-
 # Test the validate_price function
 def test_price_int_input_0_input():
     # Test case 5: Price 0
